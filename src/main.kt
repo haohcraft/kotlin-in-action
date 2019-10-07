7c12bc7d15e312efbd101273ef7ebc77c08d0367
Hao Huang <haohcraft@gmail.com>
2019-10-07 18:30:58 -0400

    JvmName annotation, package name

--- src/main.kt
+++ src/main.kt
@@ -1,21 +1,8 @@
+import strings.joinToString
+
 fun main() {
     val list = listOf<Int>(1, 2, 3, 4)
     println(joinToString(collection = list, separator = "; ", prefix = "(", postfix = ")"))
     println(joinToString(collection = list))
 }
 
-fun <T> joinToString(
-    collection: Collection<T>,
-    separator: String = ", ",
-    prefix: String = "",
-    postfix: String = ""
-): String {
-    val result = StringBuilder(prefix)
-    for ((index, element) in collection.withIndex()) {
-        if (index > 0) result.append(separator)
-        result.append(element)
-
-    }
-    result.append(postfix)
-    return result.toString()
-}
