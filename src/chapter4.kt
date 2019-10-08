bf40b81cd92afe2f8acf7f4bcd746549f6f32891
Hao Huang <haohcraft@gmail.com>
2019-10-08 17:57:13 -0400

    ch4

--- src/chapter4.kt
+++ src/chapter4.kt
@@ -57,8 +57,113 @@ fun TalkativeButton.giveSpeech() {
  * KT nested classes don't have access to the outer class instance
  */
 
+/**
+ * 4.1.5 sealed classes: defining restricted class hierarchies
+ */
 
-class Button: Clickable {
+sealed class Expr {
+    class Num(val value: Int): Expr()
+    class Sum(val left: Expr, val right: Expr): Expr()
+}
+
+fun eval(e: Expr): Int =
+    when (e) { // the "when" expression covers all possible cases, so no "else" branch is needed
+        is Expr.Num -> e.value
+        is Expr.Sum -> eval(e.right) + eval(e.left)
+    }
+
+/**
+ * 4.2 declaring a class with nontrivial constructors or properties
+ */
+
+// 4.2.1 primary constructor and initializer blocks
+// If your class has a superclass, the primary constructor also needs to initialize the superclass.
+open class User(val username: String) // primary constructor; "val" means the property will be initialized with "username"
+class TwitterUser(nickname: String) : User(nickname)
+
+// the default constructor without arguments is generated
+// note: interfaces don't have constructors
+class RadioButton: Button()
+
+/** 4.2.2 secondary constructors **/
+// If the class has no primary constructor, then each secondary constructor has to initialize the base class or delegate to another constructor that does so.
+
+/*
+
+class User constructor(_username: String) {
+    val nickname: String
+    init {
+        username = _username
+    }
+}
+ */
+
+/** 4.2.3 Implementing properties declared in interfaces **/
+interface UserInterface {
+    val nickname: String
+}
+
+class PrivateUser(override val nickname: String) : UserInterface
+class SubscribingUser(val email: String): UserInterface {
+    override val nickname: String
+        get() = email.substringBefore("@")
+}
+class FacebookUser(val accountId: Int): UserInterface {
+    override val nickname: String = getFbName(accountId)
+
+    private fun getFbName(accountId: Int): String = "name_collection"[accountId].toString()
+
+}
+
+/** 4.2.4 Accessing a backing field from a getter or setter**/
+// the special identifier "field" to access the value of the backing field
+
+/** 4.3 compiler-generated methods: data classes and class delegation **/
+/** 4.3.1 Universal Object methods **/
+data class Client(val username: String, val postalCode: Int)
+// it will compile to JAVA class containing equals, hashCode , and toString
+
+/** 4.3.2 class delegation: using the "by" keywords **/
+class CountingSet<T>(val innerSet: MutableCollection<T> = HashSet<T>()) : MutableCollection<T> by innerSet {
+    var objectsAdded = 0
+
+    override fun add(element: T): Boolean {
+        objectsAdded++
+        return innerSet.add(element)
+    }
+
+    override fun addAll(elements: Collection<T>): Boolean {
+        objectsAdded += elements.size
+        return innerSet.addAll(elements)
+    }
+}
+
+/**
+ *  4.4 Object keyword
+ *  - to define a singleton
+ *  - companion object
+ *  - object expression
+ *
+ * **/
+
+/** 4.4.1 Object declarations: singleton made easy **/
+object Payroll {
+    val allEmployees = arrayListOf<Person>()
+
+    fun calculateSalary() {
+        TODO("")
+    }
+
+}
+
+class Person
+
+/** 4.4.2 companion objects: a place for factory methods and static members **/
+/** 4.4.4. Object expressions: anonymous inner classes rephrased **/
+
+
+
+open class Button: Clickable {
     override fun click() {
         println("i was clicked")
     }
